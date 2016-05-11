84c288567f1027ea10daa136466ed4be17eeae35
xugc <guchengxu1988@163.com>
2016-05-11 21:25:08 +0800

    spring boot 最新版本

--- src/test/java/com/boot/spring/BootStarterApplicationTests.java
+++ src/test/java/com/boot/spring/BootStarterApplicationTests.java
@@ -1,54 +1,31 @@
 package com.boot.spring;
 
-import org.junit.Before;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.boot.context.web.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import com.boot.spring.service.UserService;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-// Enable JMX so we can test the MBeans (you can't do this in a properties file)
-@TestPropertySource(properties = { "spring.jmx.enabled:true",
-		"spring.datasource.jmx-enabled:true" })
-@ActiveProfiles("scratch")
-@SpringApplicationConfiguration(BootStarterApplication.class)
-//@ContextConfiguration(locations = { "/spring/applicationContext-memcached.xml" })  
-// Separate profile for web tests to avoid clashing databases
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext
 public class BootStarterApplicationTests {
 
-	@Autowired
-	private WebApplicationContext context;
-
-	private MockMvc mvc;
-	
-	@Autowired
-	private UserService userService;
-
-	@Before
-	public void setUp() {
-		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
-	}
+	@LocalServerPort
+	private int port;
 
 	@Test
-	public void testHome() throws Exception {
-
-//		this.mvc.perform(get("/")).andExpect(status().isOk())
-//				.andExpect(content().string("Bath"));
-	}
-
-	@Test
-	public void testJmx() throws Exception {
-//		assertThat(ManagementFactory.getPlatformMBeanServer()
-//				.queryMBeans(new ObjectName("jpa.sample:type=ConnectionPool,*"), null))
-//						.hasSize(1);
+	public void testJspWithEl() throws Exception {
+		ResponseEntity<String> entity = new TestRestTemplate()
+				.getForEntity("http://localhost:" + this.port, String.class);
+		Assertions.assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		Assertions.assertThat(entity.getBody()).contains("/resources/text.txt");
 	}
 
 }
