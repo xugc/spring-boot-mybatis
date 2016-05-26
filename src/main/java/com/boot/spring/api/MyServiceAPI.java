b31cdc6e5262f1d70a8edf5714df51ca9d0f7064
xugc <guchengxu1988@163.com>
2016-05-26 14:56:24 +0800

    修复启动失败bug

---
+++ src/main/java/com/boot/spring/api/MyServiceAPI.java
@@ -0,0 +1,21 @@
+/**
+ * @Title: MyServiceAPI.java
+ * @Package com.boot.spring.api
+ * @Description: TODO
+ * Copyright: Copyright (c) 2016 
+ * Company:*******
+ * 
+ * @author 徐故成
+ * @date 2016-5-26 下午2:55:15
+ * @version V1.0
+ */
+package com.boot.spring.api;
+
+/**  
+ * @ClassName: MyServiceAPI  
+ * @Description: TODO  
+ *    
+ */
+public interface MyServiceAPI {
+
+}
