9ec7f1c9a9bb8d790d86476cdfaec514a671e5a5
xugc <guchengxu1988@163.com>
2016-06-08 11:36:42 +0800

    加入CMS系统

---
+++ src/main/java/com/boot/spring/web/AdminLoginController.java
@@ -0,0 +1,31 @@
+/**
+ * @Title: AdminLoginController.java
+ * @Package com.boot.spring.web
+ * @Description: TODO
+ * Copyright: Copyright (c) 2016 
+ * Company:*******
+ * 
+ * @author 徐故成
+ * @date 2016-6-8 下午2:55:11
+ * @version V1.0
+ */
+package com.boot.spring.web;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName: AdminLoginController
+ * @Description: TODO
+ * 
+ */
+@Controller
+@EnableAutoConfiguration
+@RequestMapping("/admin")
+public class AdminLoginController {
+	@RequestMapping("/login")
+	public String login() {
+		return "admin_login";
+	}
+}
